15943d8b4a0786e1ef1b6779660a71cd8bda11e1
Geannis1 <103528295+Geannis1@users.noreply.github.com>
2023-07-06 17:11:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,32 @@ let cells = document.querySelectorAll('td');
 let displayContainer = document.getElementById('displayContainer');
 let turn = 'X'; /* we use this variable to know if it's X's turn or 0's turn */
 let inPlay = 1; /*as long as inPlay == 1 the game is on. When a winner is decided
-                we will automatically set inPlay as 0 and the game will be over*/
-cells.forEach(cell => {
-  cell.addEventListener('click', function() {
+ we will automatically set inPlay as 0 and the game will be over*/
+let table = document.getElementById('myTable');
+rows = table.rows; 
+
+ function check(cell) {
     if(inPlay == 1) {
-        if(turn == 'X' && cell.textContent == ''){
-            cell.textContent = 'X';
-        } else if(turn == '0' && cell.textContent == '') {
-            cell.textContent = '0';
-        }
+    if(turn == 'X' && cell.textContent == ''){
+        cell.textContent = 'X';
+    } else if(turn == '0' && cell.textContent == '') {
+        cell.textContent = '0';
+    }
     if(turn == 'X') {
         turn = '0';
     } else if (turn == '0') {
         turn = 'X';
     }
-    let table = document.getElementById('myTable');
-    rows = table.rows; 
-        // Check rows
+    checkWinner();
+}
+
+function checkWinner() {
+    checkVertically();
+    checkHorizontally();
+    checkDiagonals();
+}
+
+function checkHorizontally() {
     for (let i = 0; i < 3; i++) {
         if (rows[i].cells[0].innerHTML !== '' && rows[i].cells[0].innerHTML === rows[i].cells[1].innerHTML && rows[i].cells[0].innerHTML === rows[i].cells[2].innerHTML) {
             inPlay = 0;
@@ -27,10 +36,12 @@ cells.forEach(cell => {
             } else {
                 turn = 'X';
             }
-        return displayContainer.innerHTML = `Winner is: ${turn}`;
+            return displayContainer.innerHTML = `Winner is: ${turn}`;
+            }
         }
-    }
-        // Check columns
+}
+
+function checkVertically() {
     for (let j = 0; j < 3; j++) {
         if (rows[0].cells[j].innerHTML !== '' && rows[0].cells[j].innerHTML === rows[1].cells[j].innerHTML && rows[0].cells[j].innerHTML === rows[2].cells[j].innerHTML) {
             inPlay = 0;
@@ -39,10 +50,12 @@ cells.forEach(cell => {
             } else {
                 turn = 'X';
             }
-        return displayContainer.innerHTML = `Winner is: ${turn}`;
+            return displayContainer.innerHTML = `Winner is: ${turn}`;
         }
     }
-        // Check diagonals
+}
+
+function checkDiagonals() {
     if (rows[0].cells[0].innerHTML !== '' && rows[0].cells[0].innerHTML === rows[1].cells[1].innerHTML && rows[0].cells[0].innerHTML === rows[2].cells[2].innerHTML) {
         inPlay = 0;
         if(turn == 'X') {
@@ -63,8 +76,7 @@ cells.forEach(cell => {
         return displayContainer.innerHTML = `Winner is: ${turn}`;
     }   
 }
-  });
-});
+}   
 
 function reset(){
     cells.forEach(cell => {
